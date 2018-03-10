54deb119209fcc626cc007bb12c01d4fa9376bf7
GADIOUX sebastien <gadioux@informatique.univ-paris-diderot.fr>
2018-03-10 20:52:27 +0100

    Trying to write a main for testing

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,11 @@
 public class Main {
 	
+	public static void main(String[] args){
+		Dice d = new Dice(6, false);
+		String[] players = {"Alpha","Delta","Gamma","Echo","Blade","Fire"};
+		Game g = new Game(players, 100, d, 0.2 , 0.1);
+		g.play();
+	
+	}
+
 }
