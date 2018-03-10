3c56e1c8089a38ca74c49bb5e512493f7d1c6c48
GADIOUX sebastien <gadioux@informatique.univ-paris-diderot.fr>
2018-03-10 18:38:37 +0100

    Dice ready

--- src/Dice.java
+++ src/Dice.java
@@ -1,3 +1,15 @@
 public class Dice {
 	
+	private int side;
+	private boolean tricked;
+
+	public Dice(int s, boolean t){
+		this.side = s;
+		this.tricked = t;
+	}
+
+	public int roll(boolean b){
+		int r = (int)(Math.random()*side + (tricked ? 0 : 1));
+		return r;
+	}	
 }
