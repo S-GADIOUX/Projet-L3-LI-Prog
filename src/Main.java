285f9d3b932214ad7bf2ff806b1f8f9b2c868df8
GADIOUX sebastien <sebgad19@gmail.com>
2018-03-14 14:35:06 +0100

    Removing harcode in Main

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,18 @@ import com.sgadioux.style.*;
 public class Main {
 	
 	public static void main(String[] args){
-		Dice d = new Dice(6, false);
+		int dSize = 6;
+		boolean trickedDice = false
+		Dice d = new Dice(dSize, trickedDice);
+		int bLength = 100;
+		int clueNb = 3;
+		int tryNb = 3;
+		int errorZone = 10;
+		double relaunchTile = 0.2;
+		double backTile = 0.1;
+		String path = "../vec0.txt";
 		String[] players = {"Alpha","Delta","Gamma","Echo","Blade","Fire"};
-		Game g = new Game(players, 100, d, 3, 3, 10, 0.2 , 0.1, new WordVector("../vec0.txt"));
+		Game g = new Game(players, bLength, d, clueNb, tryNb, errorZone, relaunchTile, backTile, new WordVector(path));
 		g.play();
 	
 	}
