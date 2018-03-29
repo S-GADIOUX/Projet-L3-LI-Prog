f86b90be4cc1cc19d66908013804a03a9bfdc1f2
GADIOUX sebastien <sebgad19@gmail.com>
2018-03-29 15:28:12 +0200

    Removing some hardCoding in main

--- src/Main.java
+++ src/Main.java
@@ -3,28 +3,60 @@ public class Main {
 	
 	public static void main(String[] args){
 		boolean b = true;
-		int dSize = 6;
+		int len = args.length;
+		int i = 0;
+		int nbPlayer = 0;
+		String path = "";
+		int clueNb = 3;
+		int tryNb = 3;
+		int errorZone = 10;
+		int bLength = 100; //To unHard
+		double relaunchTile = 0.2; //To unHard
+		double backTile = 0.1; //To unHard
+		int dSize = 6; //To unHard
 		boolean trickedDice = false;
+		while (len > i){
+			switch (args[i]){
+				case("--w2v") :
+					i++;
+					path = args[i];	
+					break;
+				case("--nbJoueurs"):
+					i++;
+					nbPlayer = Integer.parseInt(args[i]);
+					break;
+				case("--nbTry"):
+					i++;
+					tryNb = Integer.parseInt(args[i]);
+					break;
+				case("--de_magique"):
+					trickedDice = true;
+					break;
+				case("-k"): 
+					i++;
+					errorZone = Integer.parseInt(args[i]);
+					break;
+				case("--nbClue"):
+					i++;
+					clueNb = Integer.parseInt(args[i]);
+					break;
+			}
+			i++;
+		
+		}
 		Dice d = new Dice(dSize, trickedDice);
-		int bLength = 100;
 		if (bLength<1) {
 			System.out.println("Board Length must be positive");
 			b = false;
 		}
-		int clueNb = 3;
-		int tryNb = 3;
-		int errorZone = 10;
-		double relaunchTile = 0.2;
-		double backTile = 0.1;
-		String path = args[0];
 		WordVector datas = null;
 		try {
 			datas = new WordVector(path);
 		}
 		catch(Exception e){
 			b = false;
+			System.out.println("Error in file : ");	
 			e.printStackTrace();
-			System.out.println("Error in file");	
 		}
 		String[] players = {"Alpha","Delta","Gamma","Echo","Blade","Fire"};
 		if(datas != null){
